fcca4d0a58feb4ad9435d0d21d7f585ea9063f84
Alireza Abrehforoush <wwe_alireza.abc2015@yahoo.com>
2021-11-09 16:05:37 +0330

    Microprocessor HW2 problem 1

---
+++ Microprocessor/2/1/main.asm
@@ -0,0 +1,13 @@
+;
+; 1.asm
+;
+; Created: 11/9/2021 1:53:20 PM
+; Author : Alireza Abrehforoush
+;
+
+
+; Replace with your application code
+.ORG 100
+LDI R16, 0b00010011
+LDI R17, 0b11101110
+ADD R16, R17
