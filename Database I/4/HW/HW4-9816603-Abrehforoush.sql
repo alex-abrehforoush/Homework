97634b744d8e95c2ae2257d366e99d476951e6c2
Alireza Abrehforoush <wwe_alireza.abc2015@yahoo.com>
2021-11-18 18:43:50 +0330

    DBHW4 problem 1

---
+++ Database I/4/HW/HW4-9816603-Abrehforoush.sql
@@ -0,0 +1,40 @@
+---- DATABASE DESIGN 1 @ IUT
+---- YOUR NAME: Alireza Abrehforoush
+---- YOUR STUDENT NUMBER: 9816603
+
+
+---- Q1
+---A
+
+---B
+
+---- Q2
+---A
+
+---B
+
+---C
+
+---D
+
+---- Q4
+---A
+
+---B
+
+---C
+
+---- Q5
+---A
+
+---B
+
+---C
+
+---D
+
+---- Q6
+
+---- Q7
+
+---- Q8
