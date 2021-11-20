5c54502edd89be55370df18c9f099f6809770ee7
Alireza Abrehforoush <wwe_alireza.abc2015@yahoo.com>
2021-11-21 00:04:41 +0330

    DBHW4

--- Database I/4/HW/HW4-9816603-Abrehforoush.sql
+++ Database I/4/HW/HW4-9816603-Abrehforoush.sql
@@ -2,39 +2,59 @@
 ---- YOUR NAME: Alireza Abrehforoush
 ---- YOUR STUDENT NUMBER: 9816603
 
-
----- Q1
----A
-
----B
-
----- Q2
----A
-
----B
-
----C
-
----D
-
----- Q4
----A
-
----B
-
----C
-
----- Q5
----A
-
----B
-
----C
-
----D
-
----- Q6
-
 ---- Q7
-
----- Q8
+---A
+alter table
+	student
+add column
+	GPA NUMERIC
+---B
+with table1(id, total_score) as
+(
+select
+	takes.id,
+	sum
+	( 
+	case takes.grade
+		when 'A+' then 4
+		when 'A ' then 4
+		when 'A-' then 4
+		when 'B+' then 3
+		when 'B ' then 3
+		when 'B-' then 3
+		when 'C+' then 2
+		when 'C ' then 1
+		else 0
+	end
+ * (select
+		course.credits
+	from
+		course
+	WHERE
+		course.course_id = takes.course_id)
+	)
+FROM
+	takes 
+GROUP BY
+	takes.id
+),
+table2(id, total_credit) as
+(
+select
+	id,
+	sum(credits)
+from
+	takes INNER JOIN course ON course.course_id = takes.course_id
+group by
+	id
+)
+update
+	student 
+set
+	GPA = (
+		select
+			scores / credits
+		from sum_scores as S INNER JOIN sum_credits as C ON S.id = C.id
+		WHERE
+			C.id = student."id"
+		);
