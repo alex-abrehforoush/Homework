b951f92261ded909d6d69b85a6a6d0bd4c29f473
Alireza Abrehforoush <wwe_alireza.abc2015@yahoo.com>
2022-02-14 00:43:06 +0330

    Exercises of S1, S2, S3(half) of Foundation of Computer Vision

---
+++ Foundations of Computer Vision/Exercise/s1.m
@@ -0,0 +1,16 @@
+clc
+clear
+close all
+f = fopen("img/sample_5184×3456.pgm");
+file_contant = fread(f, "uint8");
+image_data = file_contant(18:end);
+I = zeros(3456, 5184);
+k = 1;
+for i = 1: size(I, 1)
+    for j = 1: size(I, 2)
+        I(i, j) = image_data(k);
+        k = k + 1;
+    end
+end
+imshow(I, []);
+fclose(f);
---
+++ Foundations of Computer Vision/Exercise/s3.m
@@ -0,0 +1,57 @@
+%clearing command window and workspace and closing all open figures
+clc
+clear
+close all
+%reading image and storing it in a matrix
+I = imread("img\pic4.jpg");
+D = im2double(I);
+%extracting rgb colors
+R = I(:, :, 1);
+G = I(:, :, 2);
+B = I(:, :, 3);
+
+% %showing original image
+% imshow(I, []);
+% %showing extracted rgb images in grayscale in seperate figure
+% figure, imshow([R G B], []);
+
+% %concating rgb channels to make the original picture
+% J = cat(3, R, G, B);
+% imshow(J);
+
+% %swapping different channels
+% J_1 = cat(3, G, R, B);
+% J_2 = cat(3, R, B, G);
+% J_3 = cat(3, B, G, R);
+% figure, imshow([I J_1; J_2 J_3], []);
+
+% %generating negative images
+% R_N = 255 - R;
+% G_N = 255 - G;
+% B_N = 255 - B;
+% I_N = cat(3, R_N, G_N, B_N);
+% figure, imshow([I I_N], []);
+
+% %convert to grayscale(uint)
+% I_GS = (R + G + B) / 3;
+% figure, imshow(I_GS, []);
+
+% %convert to grayscale(double)
+% I_GS = (im2double(R) + im2double(G) + im2double(B)) / 3;
+% figure, imshow(I_GS, []);
+
+% %grayscale to black and white
+% K = rgb2gray(im2double(I));
+% BW_1 = K > 0.2;
+% BW_2 = K > 0.4;
+% BW_3 = K > 0.8;
+% figure, imshow([K BW_1 BW_2 BW_3], []);
+
+% %flip horizontal
+% I_FH = D;
+% for j = 1: size(D, 2)
+%     I_FH(:, size(D, 2) - j + 1, :) = D(:, j, :);
+% end
+% imshow([D I_FH; I_FH D], []);
+
+% 
