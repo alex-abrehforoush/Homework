3b11196d04e63ec0dc942222672f660bfbece9e1
Alireza Abrehforoush <wwe_alireza.abc2015@yahoo.com>
2022-02-21 00:35:41 +0330

    rotatePixel seems good

--- Foundations of Computer Vision/HW1/sources/p3.m
+++ Foundations of Computer Vision/HW1/sources/p3.m
@@ -5,17 +5,35 @@ close all
 %reading image data
 I = imread("images\Test_01.ppm");
 %defining variables
-row = 852;
-column = 1282;
-theta = 60;
-
-J = zeros(row, column, 3);
-for i = 1: size(I, 1)
-    for j = 1: size(I, 2)
-        [i_p, j_p] = rotatePixel(i, j, theta);
-        J(i_p, j_p, 1) = I(i, j, 1);
-        J(i_p, j_p, 2) = I(i, j, 2);
-        J(i_p, j_p, 3) = I(i, j, 3);
+row = size(I, 1);
+column = size(I, 2);
+theta = 280;
+new_row = ceil(abs(column * sind(theta)) + abs(row * cosd(theta)));
+new_column = ceil(abs(row * sind(theta)) + abs(column * cosd(theta)));
+%enlarging the dimensions
+K = uint8(zeros(new_row, new_column, 3));
+for i = 1: size(K, 1)
+    for j = 1: size(K, 2)
+        if (i <= round(abs(new_row - row) / 2)) || (j <= round(abs(new_column - column) / 2)) || (i > size(K, 1) - round(abs(new_row - row) / 2)) || (j > size(K, 2) - round(abs(new_column - column) / 2))
+            K(i, j, 1) = 0;
+            K(i, j, 2) = 0;
+            K(i, j, 3) = 0;
+        else
+            K(i, j, 1) = I(i - round(abs((new_row - row) / 2)), j - round(abs((new_column - column) / 2)), 1);
+            K(i, j, 2) = I(i - round(abs((new_row - row) / 2)), j - round(abs((new_column - column) / 2)), 2);
+            K(i, j, 3) = I(i - round(abs((new_row - row) / 2)), j - round(abs((new_column - column) / 2)), 3);
+        end
     end
 end
-imshow(J, []);
+J = uint8(zeros(new_row, new_column, 3));
+for i = 1: size(J, 1)
+    for j = 1: size(J, 2)
+        [i_p, j_p] = rotatePixel(new_row, new_column, i, j, -1 * theta);
+        if (i_p >= 1 && i_p <= new_row) && (j_p >= 1 && j_p <= new_column)
+            J(i, j, 1) = K(i_p, j_p, 1);
+            J(i, j, 2) = K(i_p, j_p, 2);
+            J(i, j, 3) = K(i_p, j_p, 3);
+        end
+    end
+end
+imtool(J, []);
--- Foundations of Computer Vision/HW1/sources/rotatePixel.m
+++ Foundations of Computer Vision/HW1/sources/rotatePixel.m
@@ -1,8 +1,7 @@
 %defining function to calculate new coordinates
-function [i_p, j_p] = rotatePixel(i, j, theta)
+function [i_p, j_p] = rotatePixel(row, column, i, j, theta)
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %defining variables
-    row = 852;
-    column = 1282;
     x0 = 0;
     y0 = 0;
     x1 = 0;
@@ -24,20 +23,17 @@ function [i_p, j_p] = rotatePixel(i, j, theta)
     x1 = x0 * cosd(theta) - y0 * sind(theta);
     y1 = x0 * sind(theta) + y0 * cosd(theta);
     %converting to pixel coordinate
-    r = mod(theta, 360);
-    x2 = round(abs(x1));
-    y2 = round(abs(y1));
-    if (r >= 0) && (r < 90)
-        j_p = 1 + x2 + column / 2;
-        i_p = 1 + row / 2 - y2;
-    elseif (r >= 90) && (r < 180)
-        j_p = 1 + column / 2 - x2;
-        i_p = 1 + row / 2 - y2;
-    elseif (r >= 180) && (r < 270)
-        j_p = 1 + column / 2 - x2;
-        i_p = 1 + y2 + row / 2;
+    x2 = abs(x1);
+    y2 = abs(y1);
+    if (x1 >= 0)
+        j_p = round(column / 2 + x2);
     else
-        j_p = x2 + column / 2;
-        i_p = y2 + row / 2;
+        j_p = round(column / 2 - x2);
     end
+    if (y1 >= 0)
+        i_p = round(row / 2 - y2);
+    else
+        i_p = round(row / 2 + y2);
+    end
+
 end
