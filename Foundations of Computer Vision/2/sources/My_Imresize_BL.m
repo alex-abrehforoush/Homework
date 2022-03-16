ec19a243e1a54755f2e6b910e56d928adfb134f3
Alireza Abrehforoush <wwe_alireza.abc2015@yahoo.com>
2022-03-16 16:30:59 +0330

    AIHW1

---
+++ Foundations of Computer Vision/2/sources/My_Imresize_BL.m
@@ -0,0 +1,7 @@
+function [outputArg1,outputArg2] = My_Imresize_BL(inputArg1,inputArg2)
+%MY_IMRESIZE_BL Summary of this function goes here
+%   Detailed explanation goes here
+outputArg1 = inputArg1;
+outputArg2 = inputArg2;
+end
+
