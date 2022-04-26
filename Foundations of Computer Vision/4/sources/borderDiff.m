ed9893102f6048643486468f9ca09959aacb49e9
Alireza Abrehforoush <wwe_alireza.abc2015@yahoo.com>
2022-04-27 00:39:31 +0430

    CVHW4 problem 3 part a

---
+++ Foundations of Computer Vision/4/sources/borderDiff.m
@@ -0,0 +1,13 @@
+function diff = borderDiff(X, Y, direction)
+%BORDERDIFF Summary of this function goes here
+    X = int64(X);
+    Y = int64(Y);
+    r = size(X, 1);
+    diff = uint64(0);
+    if (direction == 0)
+        diff = sum((X(r, :) - Y(1, :)) .^ 2);
+    else
+        diff = sum((X(:, r) - Y(:, 1)) .^ 2);
+    end
+end
+
---
+++ Foundations of Computer Vision/4/sources/p3b.m
@@ -0,0 +1,55 @@
+clc
+clear
+close all
+imtool close all
+%%%%%%%%%%%%%%%%
+temp = imread("images\Q3\Puzzle_2_160\Corner_1_1.tif");
+r = size(temp, 1);
+temp = imread("images\Q3\Puzzle_2_160\Shuffled_Patches.tif");
+row = size(temp, 1);
+column = size(temp, 2);
+number_of_pieces = (row / r) * (column / r) - 4;
+C11 = imread("images\Q3\Puzzle_2_160\Corner_1_1.tif");
+C18 = imread("images\Q3\Puzzle_2_160\Corner_1_16.tif");
+C51 = imread("images\Q3\Puzzle_2_160\Corner_10_1.tif");
+C58 = imread("images\Q3\Puzzle_2_160\Corner_10_16.tif");
+J = uint8(zeros(row, column, 3));
+J(1: r, 1: r, :) = C11;
+J(1: r, column - r + 1: column, :) = C18;
+J(row - r + 1: row, 1: r, :) = C51;
+J(row - r + 1: row, column - r + 1: column, :) = C58;
+%imtool(J);
+patches = uint8(zeros(number_of_pieces, r, r, 3));
+for i = 1: number_of_pieces
+    patches(i, :, :, :) = imread(['images\Q3\Puzzle_2_160\' 'Patch_' num2str(i) '.tif']);
+end
+solution = zeros(row / r, column / r);
+for i = 1: r: row
+    for j = 1: r: column
+        if (((i == 1) && (j == 1)) || ((i == row - r + 1) && (j == 1)) || ((i == 1) && (j == column - r + 1)) || ((i == row - r + 1) && (j == column - r + 1)))
+            continue;
+        end
+        if (i == 1)
+            base = J(i: i + r - 1, j - r: j - 1, :);
+            values = uint32(zeros(1, number_of_pieces));
+            for k = 1: number_of_pieces
+                values(k) = borderDiff(rgb2gray(base), rgb2gray(squeeze(patches(k, :, :, :))), 1);
+            end
+            [min_value, min_index] = min(values);
+            J(i: i + r - 1, j: j + r - 1, :) = patches(min_index, :, :, :);
+            solution(ceil(i / r), ceil(j / r)) = min_index;
+            imtool(J);
+        else
+            base = J(i - r: i - 1, j: j + r - 1, :);
+            values = uint32(zeros(1, number_of_pieces));
+            for k = 1: number_of_pieces
+                values(k) = borderDiff(rgb2gray(base), rgb2gray(squeeze(patches(k, :, :, :))), 0);
+            end
+            [min_value, min_index] = min(values);
+            J(i: i + r - 1, j: j + r - 1, :) = patches(min_index, :, :, :);
+            solution(ceil(i / r), ceil(j / r)) = min_index;
+            imtool(J);
+        end
+    end
+end
+
