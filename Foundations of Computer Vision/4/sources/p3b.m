011cc7d6f215f557ecca1563c8e9918b1f1a8e49
Alireza Abrehforoush <wwe_alireza.abc2015@yahoo.com>
2022-05-03 17:08:31 +0430

    CVHW4 almost done

--- Foundations of Computer Vision/4/sources/p3b.m
+++ Foundations of Computer Vision/4/sources/p3b.m
@@ -18,7 +18,7 @@ J(1: r, 1: r, :) = C11;
 J(1: r, column - r + 1: column, :) = C18;
 J(row - r + 1: row, 1: r, :) = C51;
 J(row - r + 1: row, column - r + 1: column, :) = C58;
-%imtool(J);
+
 patches = uint8(zeros(number_of_pieces, r, r, 3));
 for i = 1: number_of_pieces
     patches(i, :, :, :) = imread(['images\Q3\Puzzle_2_160\' 'Patch_' num2str(i) '.tif']);
@@ -38,7 +38,7 @@ for i = 1: r: row
             [min_value, min_index] = min(values);
             J(i: i + r - 1, j: j + r - 1, :) = patches(min_index, :, :, :);
             solution(ceil(i / r), ceil(j / r)) = min_index;
-            %imtool(J);
+            imshow(J, []);
         else
             base = J(i - r: i - 1, j: j + r - 1, :);
             values = uint32(zeros(1, number_of_pieces));
@@ -54,7 +54,7 @@ for i = 1: r: row
             [min_value, min_index] = min(values);
             J(i: i + r - 1, j: j + r - 1, :) = patches(min_index, :, :, :);
             solution(ceil(i / r), ceil(j / r)) = min_index;
-            %imtool(J);
+            imshow(J, []);
         end
     end
 end
--- Foundations of Computer Vision/4/sources/test.m
+++ Foundations of Computer Vision/4/sources/test.m
@@ -3,25 +3,25 @@ clear
 close all
 imtool close all
 %%%%%%%%%%%%%%%%
-temp = imread("images\Puzzle\Puzzle_5_160\Corner_1_1.tif");
+temp = imread("images\Puzzle\Puzzle_3_40\Corner_1_1.tif");
 r = size(temp, 1);
-temp = imread("images\Puzzle\Puzzle_5_160\Shuffled_Patches.tif");
+temp = imread("images\Puzzle\Puzzle_3_40\Shuffled_Patches.tif");
 row = size(temp, 1);
 column = size(temp, 2);
 number_of_pieces = (row / r) * (column / r) - 4;
-C11 = imread("images\Puzzle\Puzzle_5_160\Corner_1_1.tif");
-C18 = imread("images\Puzzle\Puzzle_5_160\Corner_1_16.tif");
-C51 = imread("images\Puzzle\Puzzle_5_160\Corner_10_1.tif");
-C58 = imread("images\Puzzle\Puzzle_5_160\Corner_10_16.tif");
+C11 = imread("images\Puzzle\Puzzle_3_40\Corner_1_1.tif");
+C18 = imread("images\Puzzle\Puzzle_3_40\Corner_1_8.tif");
+C51 = imread("images\Puzzle\Puzzle_3_40\Corner_5_1.tif");
+C58 = imread("images\Puzzle\Puzzle_3_40\Corner_5_8.tif");
 J = uint8(zeros(row, column, 3));
 J(1: r, 1: r, :) = C11;
 J(1: r, column - r + 1: column, :) = C18;
 J(row - r + 1: row, 1: r, :) = C51;
 J(row - r + 1: row, column - r + 1: column, :) = C58;
-%imtool(J);
+
 patches = uint8(zeros(number_of_pieces, r, r, 3));
 for i = 1: number_of_pieces
-    patches(i, :, :, :) = imread(['images\Puzzle\Puzzle_5_160\' 'Patch_' num2str(i) '.tif']);
+    patches(i, :, :, :) = imread(['images\Puzzle\Puzzle_3_40\' 'Patch_' num2str(i) '.tif']);
 end
 solution = zeros(row / r, column / r);
 for i = 1: r: row
@@ -33,28 +33,28 @@ for i = 1: r: row
             base = J(i: i + r - 1, j - r: j - 1, :);
             values = uint32(zeros(1, number_of_pieces));
             for k = 1: number_of_pieces
-                values(k) = borderDiffNew(rgb2gray(base), rgb2gray(squeeze(patches(k, :, :, :))), 1);
+                values(k) = borderDiff(rgb2gray(base), rgb2gray(squeeze(patches(k, :, :, :))), 1);
             end
             [min_value, min_index] = min(values);
             J(i: i + r - 1, j: j + r - 1, :) = patches(min_index, :, :, :);
             solution(ceil(i / r), ceil(j / r)) = min_index;
-            %imtool(J);
+            imshow(J, []);
         else
             base = J(i - r: i - 1, j: j + r - 1, :);
             values = uint32(zeros(1, number_of_pieces));
             for k = 1: number_of_pieces
-                values(k) = borderDiffNew(rgb2gray(base), rgb2gray(squeeze(patches(k, :, :, :))), 0);
+                values(k) = borderDiff(rgb2gray(base), rgb2gray(squeeze(patches(k, :, :, :))), 0);
             end
             if (j > 1)
                 base = J(i: i + r - 1, j - r: j - 1, :);
                 for k = 1: number_of_pieces
-                    values(k) = values(k) + borderDiffNew(rgb2gray(base), rgb2gray(squeeze(patches(k, :, :, :))), 1);
+                    values(k) = values(k) + borderDiff(rgb2gray(base), rgb2gray(squeeze(patches(k, :, :, :))), 1);
                 end
             end
             [min_value, min_index] = min(values);
             J(i: i + r - 1, j: j + r - 1, :) = patches(min_index, :, :, :);
             solution(ceil(i / r), ceil(j / r)) = min_index;
-            %imtool(J);
+            imshow(J, []);
         end
     end
 end
