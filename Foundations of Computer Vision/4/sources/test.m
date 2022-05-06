42116237a571c548119356af174799c185003700
Alireza Abrehforoush <wwe_alireza.abc2015@yahoo.com>
2022-05-06 21:04:54 +0430

    AIHW2_T problem 3

--- Foundations of Computer Vision/4/sources/test.m
+++ Foundations of Computer Vision/4/sources/test.m
@@ -3,16 +3,16 @@ clear
 close all
 imtool close all
 %%%%%%%%%%%%%%%%
-temp = imread("images\Puzzle\Puzzle_3_40\Corner_1_1.tif");
+temp = imread("images\Puzzle\Puzzle_5_160\Corner_1_1.tif");
 r = size(temp, 1);
-temp = imread("images\Puzzle\Puzzle_3_40\Shuffled_Patches.tif");
+temp = imread("images\Puzzle\Puzzle_5_160\Shuffled_Patches.tif");
 row = size(temp, 1);
 column = size(temp, 2);
 number_of_pieces = (row / r) * (column / r) - 4;
-C11 = imread("images\Puzzle\Puzzle_3_40\Corner_1_1.tif");
-C18 = imread("images\Puzzle\Puzzle_3_40\Corner_1_8.tif");
-C51 = imread("images\Puzzle\Puzzle_3_40\Corner_5_1.tif");
-C58 = imread("images\Puzzle\Puzzle_3_40\Corner_5_8.tif");
+C11 = imread("images\Puzzle\Puzzle_5_160\Corner_1_1.tif");
+C18 = imread("images\Puzzle\Puzzle_5_160\Corner_1_16.tif");
+C51 = imread("images\Puzzle\Puzzle_5_160\Corner_10_1.tif");
+C58 = imread("images\Puzzle\Puzzle_5_160\Corner_10_16.tif");
 J = uint8(zeros(row, column, 3));
 J(1: r, 1: r, :) = C11;
 J(1: r, column - r + 1: column, :) = C18;
@@ -21,7 +21,7 @@ J(row - r + 1: row, column - r + 1: column, :) = C58;
 
 patches = uint8(zeros(number_of_pieces, r, r, 3));
 for i = 1: number_of_pieces
-    patches(i, :, :, :) = imread(['images\Puzzle\Puzzle_3_40\' 'Patch_' num2str(i) '.tif']);
+    patches(i, :, :, :) = imread(['images\Puzzle\Puzzle_5_160\' 'Patch_' num2str(i) '.tif']);
 end
 solution = zeros(row / r, column / r);
 for i = 1: r: row
